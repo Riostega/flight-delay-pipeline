ec25cd3bebea30c714cdab12a14d7821ebdca934
Joshua Ortega-Rios <ortjoshua716@gmail.com>
2026-07-28 00:40:44 -0500

    Snowflake connection works

---
+++ test_snowflake.py
@@ -0,0 +1,18 @@
+import snowflake.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = snowflake.connector.connect(
+    account=os.getenv("SNOWFLAKE_ACCOUNT"),
+    user=os.getenv("SNOWFLAKE_USER"),
+    password=os.getenv("SNOWFLAKE_PASSWORD"),
+    warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+    database=os.getenv("SNOWFLAKE_DATABASE"),
+    schema=os.getenv("SNOWFLAKE_SCHEMA")
+)
+
+cur = conn.cursor()
+cur.execute("SELECT CURRENT_VERSION()")
+print(cur.fetchone())
